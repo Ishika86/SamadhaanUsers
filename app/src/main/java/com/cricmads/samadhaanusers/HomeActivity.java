e18b9401ed3387e20c6930b832a5450d014c6bdb
Adesh Fafriya <adeshfafriya@gmail.com>
2019-03-02 11:05:41 +0530

    adding category and ministry

--- app/src/main/java/com/cricmads/samadhaanusers/HomeActivity.java
+++ app/src/main/java/com/cricmads/samadhaanusers/HomeActivity.java
@@ -2,6 +2,7 @@ package com.cricmads.samadhaanusers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,10 +14,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private TextView applicationFiledTxt, totalAmountTxt, mutually_settledTxt, applicationPendingTxt, applicationRejectedTxt, applicationDisposedTxt;
+    private int applicationFiled=0, totalAmount=0, mutually_settled=0, applicationPending=0, applicationRejected=0, applicationDisposed=0;
+    DatabaseReference dataref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +35,56 @@ public class HomeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.Samadhaan);
+        dataref = FirebaseDatabase.getInstance().getReference().child("applicationsData");
+        applicationFiledTxt = findViewById(R.id.applicationFiled);
+        totalAmountTxt = findViewById(R.id.totalAmount);
+        mutually_settledTxt = findViewById(R.id.mutually_settled);
+        applicationPendingTxt = findViewById(R.id.applicationPending);
+        applicationRejectedTxt = findViewById(R.id.applicationRejected);
+        applicationDisposedTxt = findViewById(R.id.applicationDisposed);
+
+        dataref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot applicationSnapshot : dataSnapshot.getChildren()){
+                    String AppFiled = applicationSnapshot.child("ApplicationFiled").getValue(String.class);
+                    int appFiledNo = Integer.parseInt(AppFiled);
+                    applicationFiled = applicationFiled+appFiledNo;
+
+                    String AppDisposed = applicationSnapshot.child("Applications disposed by MSEFC Council").getValue(String.class);
+                    int appDisposedNo = Integer.parseInt(AppDisposed);
+                    applicationDisposed = applicationDisposed+appDisposedNo;
+
+                    String AppMutuallySettled = applicationSnapshot.child("Applications mutually settled").getValue(String.class);
+                    int appMutuallySettledNo = Integer.parseInt(AppMutuallySettled);
+                    mutually_settled = mutually_settled+appMutuallySettledNo;
+
+                    String AppPending = applicationSnapshot.child("Applications pending").getValue(String.class);
+                    int appPendingNo = Integer.parseInt(AppPending);
+                    applicationPending = applicationPending+appPendingNo;
+
+                    String AppRejected = applicationSnapshot.child("Applications rejected by MSEFC Council").getValue(String.class);
+                    int appRejectedNo = Integer.parseInt(AppRejected);
+                    applicationRejected = applicationRejected+appRejectedNo;
+
+                    String AppAmount = applicationSnapshot.child("Total amount involved").getValue(String.class);
+                    int appAmountNo = Integer.parseInt(AppAmount);
+                    totalAmount = totalAmount+appAmountNo;
+                }
+
+                applicationFiledTxt.setText(String.valueOf(applicationFiled));
+                totalAmountTxt.setText(String.valueOf(totalAmount));
+                mutually_settledTxt.setText(String.valueOf(mutually_settled));
+                applicationPendingTxt.setText(String.valueOf(applicationPending));
+                applicationRejectedTxt.setText(String.valueOf(applicationRejected));
+                applicationDisposedTxt.setText(String.valueOf(applicationDisposed));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
